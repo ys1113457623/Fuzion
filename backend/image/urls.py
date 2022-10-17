266bddd048d66222b02ce355066548fd26444fa7
Raj Upadhyay <rupadhyay523@gmail.com>
2022-10-18 00:34:58 +0530

    updated to single endpoint

---
+++ backend/image/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+#now import the views.py file into this code
+from . import views
+# urlpatterns=[
+#   path('',views.index),
+#   path('missWord/',views.missWord),
+#   path('classify/',views.classifyImage)
+# ]
