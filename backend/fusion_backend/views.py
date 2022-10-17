515c869e6f5cc64c4fead1767d7fd116c4e96de6
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-17 17:19:57 +0530

    Backend

---
+++ backend/fusion_backend/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+ 
+# Defining a function which
+# will receive request and
+# perform task depending
+# upon function definition
+def hello(request) :
+    # This will return Hello Geeks
+    # string as HttpResponse
+    return HttpResponse("Hello")
