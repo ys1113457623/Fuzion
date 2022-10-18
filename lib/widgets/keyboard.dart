48e735b325e8c9beac9eb53386421d26c3c3ee3e
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-18 10:56:40 +0530

    Merge fix

--- lib/widgets/keyboard.dart
+++ lib/widgets/keyboard.dart
@@ -1,4 +1,3 @@
-import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
