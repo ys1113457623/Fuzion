cf65d5c3cac229c7400da08ab53a942d32989e9c
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-19 00:57:56 +0530

    stroke

--- backend/open_ai/main.py
+++ backend/open_ai/main.py
@@ -1,12 +1,23 @@
 import json
+from multiprocessing.resource_sharer import stop
 import openai
 import requests
-API_TOKEN = "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"
+
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+
+# dotenv_path = Path('./.env')
+load_dotenv()
+API_TOKEN = os.getenv("HUGGING_FACES")
+OPEN_AI_TOKEN = os.getenv("OPEN_AI")
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
 
 def imageDescription(request):
     query = request
+    print(query)
     API_URL = "https://api-inference.huggingface.co/models/nlpconnect/vit-gpt2-image-captioning"
     response = requests.get(query)
     # data = Image.open(BytesIO(response.content))
@@ -14,8 +25,9 @@ def imageDescription(request):
         
     response = requests.request("POST", API_URL, headers=headers, data=query)
     response = response.content.decode("utf-8")
+    return response
 
-def questionAnswering(question,context):
+def questionAnswering(question:str,context:str)->str:
     payload = {
         "inputs": {
             "question": question,
@@ -27,13 +39,11 @@ def questionAnswering(question,context):
     response = requests.request("POST", API_URL, headers=headers, data=data)
     return json.loads(response.content.decode("utf-8"))
 
-class GPT3_DaVinci:
+class DavinciTasks:
+    
+    
     def __init__(self) -> None:
-        # load_dotenv(dotenv_path=Path('./.env'))
-        
-        openai.api_key = "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"
-    
-    
+        openai.api_key = OPEN_AI_TOKEN
     
     def name_generator(self,product_description:str):
         prompt = f"""
@@ -50,18 +60,33 @@ Product names:
 """Product description: A home milkshake maker
 Seed words: fast, healthy, compact.
 Product names: HomeShaker, Fit Shaker, QuickShake, Shake Maker""","")
+    
+    def extract_name_mailing(self,text):
+        pre_prompt = f"Extract the name and mailing address from this email:\n{text}\n\nName:"
+        return self.query(pre_prompt,max_tokens=64)
         
-    def query(self,prompt,temperature=0.9,max_tokens=1550,top_p=1,frequency_penalty=0,presence_penalty=0.6)->str:
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            temperature=temperature,
-            max_tokens=max_tokens,
-            top_p=top_p,
-            frequency_penalty=frequency_penalty,
-            presence_penalty=presence_penalty,
-
-        )
+    def query(self,prompt,temperature=0.9,max_tokens=1550,top_p=1,frequency_penalty=0,presence_penalty=0.6,stop=None)->str:
+        if stop:
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                temperature=temperature,
+                max_tokens=max_tokens,
+                top_p=top_p,
+                frequency_penalty=frequency_penalty,
+                presence_penalty=presence_penalty,
+                stop=stop
+            )
+            return response.choices[0].text
+        response =openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                temperature=temperature,
+                max_tokens=max_tokens,
+                top_p=top_p,
+                frequency_penalty=frequency_penalty,
+                presence_penalty=presence_penalty,
+            ) 
         return response.choices[0].text
     
     def generateCaption(self,image_url):
@@ -73,8 +98,11 @@ Product names: HomeShaker, Fit Shaker, QuickShake, Shake Maker""","")
     def correctGrammar(self,text):
         prompt = f"Correct this to standard English:\n\n\"{text}\""
         return self.query(prompt)
+    
+    def tldr(self,prompt:str):
+        text = f"{prompt}\n\nTl;dr"
+        return self.query(text,max_tokens=100)
 
-        
     def summarizeForSecondGrader(self,text,tokens = -1):
         prompt = f"Summarize this text for a second-grade:\n\n\"{text}\""
         if tokens!=-1:
@@ -83,7 +111,7 @@ Product names: HomeShaker, Fit Shaker, QuickShake, Shake Maker""","")
 
     def extractKeywords(self,text):
         prompt = f"Extract keywords from this text:\n\n\"{text}\""
-        # print(kk)
+        # print(prompt)
         return self.query(prompt)
       
     def factual_answere_generation(self,prompt:str):
@@ -168,21 +196,20 @@ A: Two, Phobos and Deimos.
         return reply.strip('\n')
     
     def ad_copy_creation(self,product_description:str):
-        targetPlatform = questionAnswering(
-        question="Where will this ad run on?",
-        context=product_description
-    )
-        targetAudience = questionAnswering(
-        question="Who is the target audience?",
-        context=product_description
-    )
         try:
-            targetAudience = targetAudience['answer']
-        except:
+            targetAudience =  questionAnswering(
+                question="Where will this ad run on?",
+                context=product_description
+            )['answer']
+        except Exception:
             targetAudience = "Everyone"
+            
         try:
-            targetPlatform = targetPlatform['answer']
-        except:
+                targetPlatform = questionAnswering(
+                question="Who is the target audience?",
+                context=product_description
+            )['answer']
+        except Exception:
             targetPlatform = "Social Media"
 
         ad_copy = f"""Write a creative ad for the following product to run on {targetPlatform} aimed at {targetAudience}:
@@ -195,19 +222,18 @@ Product: {product_description}."""
                             presence_penalty=0,
                             # max_tokens=100
                           )
+    
+
+if __name__ == "__main__":
+    DT = DavinciTasks()
+    product = "Naruto Uzumaki is a twelve-year-old ninja living in the Hidden Leaf Village under harsh ridicule from the villagers, having defaced a monument dedicated to the four Hokage leaders out of a need for acknowledgement. Naruto's teacher Iruka Umino reprimands Naruto for putting graffiti on each face statues, later hearing Naruto's aspirations of becoming Hokage to become respected by the villages. The following day, being held back from graduating the Ninja Academy after a botched clone jutsu, Naruto is tricked by an academy teacher named Mizuki into stealing a scroll containing the secret ninjutsu from the Third Hokage Hiruzen Sarutobi that night. Iruka intercepts Naruto as Mizuki was going to kill the boy and leave the village with the stolen scroll. Mizuki then reveals to Naruto that villagers were ordered not to reveal him as a host to a demon fox called the Nine Tails which devastated their village twelve years ago. Mizuki explains this to be the reasoning Naruto is hated, adding that Iruka's parents were killed by the Nine Tails. But a remorseful Iruka protects Naruto while telling the boy they have a lot in common, spurring Naruto to use the Shadow Clone jutsu he learned from the scroll to overwhelm Mizuki. Soon after Iruka uses Naruto's ability to use shadow clones to justify his graduation from the Ninja Academy."
+    # print(DT.ad_copy_creation(product))
+    print(DT.generateCaption("https://upload.wikimedia.org/wikipedia/en/3/3b/Narutofirstdvd.jpg"))
+     
+
+            
         
         
 
     
           
-# Load your API key from an environment variable or secret management service
-
-if __name__ == "__main__":
-    # gpt3 = GPT3_DaVinci()
-    # print(gpt3.query('Generate a list of insta caption for an image with this description "two men in suits posing for a picture"'))
-    context = "A smart assistant that gives the general public their taste of first state of general purpose AI, either by text or voice. That goes beyond Siri and Alexa. and gives you the feel of talking to a really smart and handy person and rather than robot"
-    gpt3 = GPT3_DaVinci()
-    # print(gpt3.ad_copy_creation(context))
-    product_description = "This is a Proudly Indian and 100% PeTA approved Vegan product from Zouk. It has a stylish print inspired by India's rich heritage. It has sturdy and durable accessories, with a zip closure and double straps (non-adjustable). The bag contains a single main compartment. This compartment has an inner padded laptop compartment that can fit up to a 15.6 inch laptop. The compartment also has an inbuilt phone, keychain and pen slot."
-    
-    print(gpt3.name_generator(product_description))
